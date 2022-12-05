373832679e1349b0b8192e8100bacfdb9bce47f3
Erik Grootoonk <erik.grootoonk@gmail.com>
2022-12-05 09:36:12 +0100

    create pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: { 
+      colors:{
+      'txtgreen':'#30B21E',
+      },
+  },
+  plugins: [],
+}
+}
