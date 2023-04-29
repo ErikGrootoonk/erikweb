03a85a92217fd70ca109e827579e29a8d48b2b73
Erik Grootoonk <erik.grootoonk@gmail.com>
2023-04-29 15:24:50 +0200

    met boekenlijst

--- src/readjson.js
+++ src/readjson.js
@@ -1,19 +1,43 @@
+  // Load the JSON file using Fetch API
 fetch('boekenlijst.json')
-  .then(response => response.json())
-  .then(function (data) {
-    // console.log(data)
-      // Create a new <ul> element
-      const ul = document.createElement('ul');
+.then(response => response.json())
+.then(data => {
+  // Get the div where we'll create the grid
+  const gridDiv = document.getElementById('grid');
+  
+  // Create a table element
+  const table = document.createElement('table');
+  
+  // Create a header row
+  const headerRow = document.createElement('tr');
+  
+  // Loop through the keys in the first object to create the header cells
+  for (let key in data[0]) {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = key;
+    headerRow.appendChild(headerCell);
+  }
+  
+  // Add the header row to the table
+  table.appendChild(headerRow);
+  
+  // Loop through the objects in the array to create the rows
+  data.forEach(object => {
+    const row = document.createElement('tr');
+    
+    // Loop through the keys in the object to create the cells
+    for (let key in object) {
+      const cell = document.createElement('td');
+      cell.textContent = object[key];
+      row.appendChild(cell);
+    }
+    
+    // Add the row to the table
+    table.appendChild(row);
+  });
+  
+  // Add the table to the grid div
+  gridDiv.appendChild(table);
+})
+.catch(error => console.error(error));
 
-      // Loop through the JSON data and create <li> elements for each item
-      data.forEach(item => {
-        // console.log(item)
-        const li = document.createElement('li');
-        li.innerText = item.voornaam;
-        ul.appendChild(li);
-      });
-
-      // Add the <ul> element to the <body> element
-      document.body.appendChild(ul);
-    })
-  .catch(error => console.error(error));
